d7d56543d5720c505f19fb4a59a6cfc1bb0f28a9
Jayy1950 <114063180+Jayy1950@users.noreply.github.com>
2022-09-30 11:19:44 +0530

    Add files via upload

---
+++ FifthQuest.java
@@ -0,0 +1,19 @@
+interface Clickable {
+    default void click() {
+        System.out.println("click");
+    }
+}
+
+interface Accessible {
+    default void access() {
+        System.out.println("access");
+    }
+}
+public class FifthQuest implements Clickable, Accessible {
+    public static void main(String[] args) {
+        FifthQuest button = new FifthQuest();
+        button.click();
+        button.access();
+    }
+
+}
